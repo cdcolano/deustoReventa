03fd64dfc416cdc3878b6cd2426bce035af5008a
Carlos del Campo <carlos.delcampo@opendeusto.es>
2022-03-28 13:12:56 +0200

    first commit

---
+++ src/main/java/com/deustoReventa/app/App.java
@@ -0,0 +1,13 @@
+package com.deustoReventa.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
