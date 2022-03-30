558d1b72bd586c48fddff9ef07ac1ce60e2bd1d7
Jon Eguiluz <jon.egiluz@opendeusto.es>
2022-03-30 13:14:54 +0200

    Clases con atributos

--- src/main/java/deustoReventa/serialization/Compra.java
+++ src/main/java/deustoReventa/serialization/Compra.java
@@ -1,5 +1,10 @@
 package deustoReventa.serialization;
 
 public class Compra {
-
+	private int id;
+	private Usuario comprador;
+	private Usuario vendedor;
+	private Producto producto;
+	private String opinion;
+	private double precio;
 }
--- src/main/java/deustoReventa/serialization/Mensaje.java
+++ src/main/java/deustoReventa/serialization/Mensaje.java
@@ -1,5 +1,11 @@
 package deustoReventa.serialization;
 
-public class Mensaje {
+import java.sql.Date;
 
+public class Mensaje {
+	private int id;
+	private String contenido;
+	private Date fecha;
+	private Usuario emisor;
+	private Usuario receptor;
 }
