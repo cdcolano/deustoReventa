dbbc585225849f8ddb00d8d52786729aa0208641
Carlos del Campo <carlos.delcampo@opendeusto.es>
2022-05-02 19:07:56 +0200

    nuevos test

--- src/main/java/dao/IProductoDAO.java
+++ src/main/java/dao/IProductoDAO.java
@@ -9,8 +9,6 @@ import serialization.ProductoVehiculo;
 public interface IProductoDAO {
 	public void storeProducto(Producto prod);
 	public List<Producto> getProductos();
-	public void deleteAllProductos();
-	public void updateProducto(Producto p);
 	public List<ProductoVehiculo> getProductosVehiculos();
 	public List<ProductoOrdenador> getProductosOrdenador();
 	public List<ProductoOrdenador> getProductosOrdenadorEnVenta();
--- src/main/java/dao/ProductoDAO.java
+++ src/main/java/dao/ProductoDAO.java
@@ -50,38 +50,6 @@ public class ProductoDAO implements IProductoDAO{
 		}
 	}
 	
-	public void deleteAllProductos() {
-		System.out.println("- Cleaning the DB...");
-		PersistenceManager pm = pmf.getPersistenceManager();
-		Transaction tx = pm.currentTransaction();
-		try {
-			tx.begin();
-			
-			// Getting ready for removing objects - Remove Relationships between User and other things
-			Extent<Producto> extentU = pm.getExtent(Producto.class, true);
-			
-		
-			// Updating the database so changes are considered before commit
-			pm.flush();
-
-			// Deleting All Products - Copies in Books will be deleted due to 'delete on cascade'
-			Query<Producto> query2 = pm.newQuery(Producto.class);
-			System.out.println(" * '" + query2.deletePersistentAll() + "' products deleted from the DB.");
-
-			tx.commit();
-		} catch (Exception ex) {
-			System.err.println(" $ Error cleaning the DB: " + ex.getMessage());
-			ex.printStackTrace();
-		} finally {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-
-			if (pm != null && !pm.isClosed()) {
-				pm.close();
-			}
-		}
-	}
 	
 	public List<Producto> getProductos() {
 		PersistenceManager pm = pmf.getPersistenceManager();
@@ -303,60 +271,10 @@ public class ProductoDAO implements IProductoDAO{
 	
 	
 	
-	public List<Producto> getProductosVendidos() {
-		PersistenceManager pm = pmf.getPersistenceManager();
-		/*
-		 * By default only 1 level is retrieved from the db so if we wish to fetch more
-		 * than one level, we must indicate it
-		 */
-		pm.getFetchPlan().setMaxFetchDepth(3);
-
-		Transaction tx = pm.currentTransaction();
-		List<Producto> productos = new ArrayList<>();
-
-		try {
-			System.out.println("   * Retrieving an Extent for Products.");
-
-			tx.begin();
-			Extent<Producto> extent = pm.getExtent(Producto.class, true);
-
-			for (Producto p : extent) {
-				if (p.isVendido()) productos.add(p);
-			}
-
-			tx.commit();
-		} catch (Exception ex) {
-			System.out.println("   $ Error retrieving an extent: " + ex.getMessage());
-		} finally {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-
-			pm.close();
-		}
-
-		return productos;
-	}
-	
 	
 
-	public void updateProducto(Producto prod) {
-		PersistenceManager pm = pmf.getPersistenceManager();
-		Transaction tx = pm.currentTransaction();
-
-		try {
-			tx.begin();
-			pm.makePersistent(prod);
-			tx.commit();
-		} catch (Exception ex) {
-			System.out.println("   $ Error updating: " + ex.getMessage());
-		} finally {
-			if (tx != null && tx.isActive()) {
-				tx.rollback();
-			}
-
-			pm.close();
-		}
+	public void setPmf(PersistenceManagerFactory pmf2) {
+		this.pmf=pmf2;
 	}
 	
 }
