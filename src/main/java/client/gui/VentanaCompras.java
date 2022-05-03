0a5332228725a8816575b7fb5e3ef0414ed310b7
Carlos del Campo <carlos.delcampo@opendeusto.es>
2022-05-04 00:26:24 +0200

    proyecto funcional

--- src/main/java/client/gui/VentanaCompras.java
+++ src/main/java/client/gui/VentanaCompras.java
@@ -21,9 +21,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import client.controller.ChatController;
 import client.controller.ComprasController;
 import client.controller.ProductoController;
 import client.controller.VentasController;
+import dao.UsuarioDAO;
 import serialization.Categoria;
 import serialization.Compra;
 import serialization.Producto;
@@ -129,7 +131,7 @@ public class VentanaCompras extends JFrame{
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				VentanaChat v= new VentanaChat(cliente, webTarget, email);
+				VentanaChat v= new VentanaChat(new ChatController(webTarget, email, new UsuarioDAO()),cliente, webTarget, email);
 				v1.dispose();	
 			}
 			
