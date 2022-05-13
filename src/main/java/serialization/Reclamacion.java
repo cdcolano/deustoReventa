d03b513456a01a50ffd377878d424f97a50c4c5f
Ander Ruiz de veye <ander.ruizdeveye@opendeusto.es>
2022-05-13 17:44:14 +0200

    clase reclamacion y lista reclamacionesHechas

---
+++ src/main/java/serialization/Reclamacion.java
@@ -0,0 +1,38 @@
+package serialization;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+public class Reclamacion {
+	@PrimaryKey
+	@Persistent(valueStrategy=IdGeneratorStrategy.INCREMENT)
+	private int id;
+	private String contenido;
+	private long fecha;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContenido() {
+		return contenido;
+	}
+	public void setContenido(String contenido) {
+		this.contenido=contenido;
+	}
+	public long getFecha() {
+		return fecha;
+	}
+	public void setFecha(long fecha) {
+		this.fecha=fecha;
+	}
+	
+	
+	public Reclamacion(String contenido) {
+		this.contenido=contenido;
+		this.fecha = System.currentTimeMillis();
+	}
+}
